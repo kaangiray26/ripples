db5a2349f2c9a2c871d7ddcc16db3284e2528987
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-03-25 15:19:07 +0100

    removed worker script

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,5 @@ import router from '/router'
 import '/scss/styles.scss'
 import '/assets/styles.css'
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/serviceworker.js");
-}
-
 // createApp(App).use(router).mount('#app');
 createApp(App).mount('#app');
